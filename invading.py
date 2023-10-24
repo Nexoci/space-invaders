a7f0f87cebcae5316c8de27d2c32d56673090ac5
Your Name <you@example.com>
2023-10-24 14:29:15 -0400

    starting to come together

--- invading.py
+++ invading.py
@@ -8,7 +8,29 @@ from buttons import imagebutton
 from static import stillimage
 from text import customtext
 
+
 pygame.init()
+def exit():
+    pygame.quit()
+    sys.exit()
+def next():
+    global done
+    done=True
+def health():
+    global healthbar
+    healthbar="images/healthbar.png"
+    
+def gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT):
+        spacer = 10
+        font = pygame.font.SysFont('Consolas', 10)
+        for gridX in range(0, WINDOW_WIDTH, spacer):        
+            window.blit(pygame.transform.rotate(font.render(str(gridX), True, (255, 255, 255)),90),(gridX,0))
+        for gridY in range(0, WINDOW_HEIGHT, spacer):
+            window.blit(font.render(str(gridY), True, (255, 255, 255)), (0, gridY))
+        for gridX in range(0, WINDOW_WIDTH, spacer):
+            pygame.draw.line(window,(255,0,0),(gridX,0),(gridX,WINDOW_HEIGHT))
+        for gridY in range(0, WINDOW_HEIGHT, spacer):
+            pygame.draw.line(window,(255,0,0),(0,gridY),(WINDOW_WIDTH,gridY)) 
 fps = 30
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 700
@@ -16,11 +38,20 @@ WINDOW_HEIGHT = 800
 player_group = pygame.sprite.Group()
 space_group = pygame.sprite.Group()
 collision_group = pygame.sprite.Group()
+start_group = pygame.sprite.Group()
+starttext_group = pygame.sprite.Group()
+start_btn= pygame.sprite.Group()
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
 billy=Player(350,600,125,75,"images/billy.png",10)
 player_group.add(billy)
 spaceback=backround(WINDOW_WIDTH,WINDOW_HEIGHT,"images/spacers.jpg")
 space_group.add(spaceback)
+btn_ext= imagebutton(225,200,250,250,"images/play.png","images/playclicked.png",next)
+start_btn.add(btn_ext)
+startback=backround(WINDOW_WIDTH,WINDOW_HEIGHT,"images/startback.jpg")
+start_group.add(startback)
+title= stillimage(125,0,500,250,"images/title.png")
+start_group.add(title)
 collidewalls=backround(WINDOW_WIDTH,WINDOW_HEIGHT,"images/collide.png")
 collision_group.add(collidewalls)
 pygame.display.set_caption("Space Invaders")
@@ -34,14 +65,18 @@ def display():
 done=False
 while not done:
     window.fill((255,255,255))
+    start_group.draw(window)
+    start_btn.draw(window)
+    #gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT)
     for event in pygame.event.get():
         pos = pygame.mouse.get_pos()
+        start_btn.update(pos,event)
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
     fpsClock.tick(fps)
-    
+
 while True:
     display()
     for event in pygame.event.get():
