2bdd1545432936172f94aaa2aa88bbb389d52825
Nexoci <144256092+Nexoci@users.noreply.github.com>
2023-11-13 17:50:28 -0500

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 class Evil(pygame.sprite.Sprite):
     def __init__(self, startX,startY,width,height,image_to_use,speed):
@@ -13,14 +14,11 @@ class Evil(pygame.sprite.Sprite):
     def move(self):
         if self.direction:
             self.rect.x-=self.speed
-            self.rect.y -= 10
         else:
             self.rect.x += self.speed 
-            self.rect.y =- 10
     def check_hit(self,group):
         if pygame.sprite.spritecollide(self,group, False, collided=pygame.sprite.collide_mask):
             self.direction=not self.direction
             return True
         else:
             return False
-    
